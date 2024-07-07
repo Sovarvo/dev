d3c20507698e3cc2cfe214a5cf26f22c0ebbeeea
Sovarvo <157177295+Sovarvo@users.noreply.github.com>
2024-07-07 22:32:03 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -60,8 +60,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "be5547665fd2a22cc913a767b32f10b9
 "icons/Icon-512.png": "ff200239f1f9d0828b65781c522d46db",
 "icons/Icon-maskable-192.png": "15a0a17f24f95cf2cacb5b2ae7d09144",
 "icons/Icon-maskable-512.png": "ff200239f1f9d0828b65781c522d46db",
-"index.html": "5288c76fd9fcac725bf7367b11c3fe17",
-"/": "5288c76fd9fcac725bf7367b11c3fe17",
+"index.html": "9514dbcab04ad7fb8ee4bf4eea2968ac",
+"/": "9514dbcab04ad7fb8ee4bf4eea2968ac",
 "main.dart.js": "835ab9b7c8344e4e4ff49a1e86e03dea",
 "manifest.json": "82d72e1cbcc19d8e151d841bc39479a8",
 "version.json": "3044ff8671c815035566efed859ca26e"};
